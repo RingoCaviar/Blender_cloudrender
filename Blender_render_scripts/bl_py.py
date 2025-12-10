6585b6252ab2075856a48187d5da951b7999c76e
Hina_Mikan <44318590+RingoCaviar@users.noreply.github.com>
2025-12-10 15:16:55 +0800

    Update bl_py.py

--- Blender_render_scripts/bl_py.py
+++ Blender_render_scripts/bl_py.py
@@ -7,7 +7,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 # 设置你的 工程文件名称
 BLEND_FILE = "shot4.blend"
 
-blend_file_path = os.path.join(script_dir, "shot4.blend")
+blend_file_path = os.path.join(script_dir, BLEND_FILE)
 # 输出文件名 在这里设置输出文件名
 OUTPUT_NAME = "babala_"
 # 生成 输出文件 绝对路径 默认放在脚本路径内的output文件夹中
@@ -34,7 +34,9 @@ def render():
         # "-s", str(START),      # 起始帧
         # "-e", str(END),        # 结束帧
         "-o", output_path, 
-        "-a"                   # 执行动画渲染 (Animation)
+        #"-a",                   # 执行动画渲染 (Animation)
+        "-f", "1",               # 执行单帧渲染，和动画渲染参数不能共存！
+        "--", "--cycles-device", "OPTIX"
     ]
     
     # 打印一下命令方便检查
