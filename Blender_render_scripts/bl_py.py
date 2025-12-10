dd601fe31e02fd7da6041a69ccce68059fa98670
Hina_Mikan <44318590+RingoCaviar@users.noreply.github.com>
2025-12-10 14:49:44 +0800

    优化渲染脚本

--- Blender_render_scripts/bl_py.py
+++ Blender_render_scripts/bl_py.py
@@ -3,26 +3,25 @@ import os
 import platform
 
 # ================= 配置区域 (修改这里) =================
-# 获取脚本 当前 路径
 script_dir = os.path.dirname(os.path.abspath(__file__))
-# 设置 工程文件 名
+# 设置你的 工程文件名称
 BLEND_FILE = "shot4.blend"
-# 生成 工程文件 绝对路径
+
 blend_file_path = os.path.join(script_dir, "shot4.blend")
-# 输出文件名
-OUTPUT_NAME = "babala"
+# 输出文件名 在这里设置输出文件名
+OUTPUT_NAME = "babala_"
 # 生成 输出文件 绝对路径 默认放在脚本路径内的output文件夹中
 output_path = os.path.join(script_dir, "output", OUTPUT_NAME)
 
-# 帧数范围
-START = 500
-END = 513
+# 帧数范围 有需要可以解除注释，记得下方的渲染命令也要解除
+# START = 500
+# END = 513
 
 GPU_SCRIPT = "setting.py"
 setting_path = os.path.join(script_dir, GPU_SCRIPT)
 
-# Blender主程序目录
-BLENDER_CMD = r"D:\ProgramFiles\blender-5.0.0-windows-x64\blender.exe" 
+# Blender主程序目录，如果是windows请自行修改
+BLENDER_CMD = r"blender" 
 # ======================================================
 
 def render():
@@ -30,15 +29,15 @@ def render():
     cmd = [
         BLENDER_CMD,           # Blender 命令
         "-b", blend_file_path,      # 后台模式 + 文件路径
-        "--factory-startup",
+        # "--factory-startup",     #使用出厂设置启动Blender，一般不需要开启
         "-P", setting_path,      # 关键：加载 GPU 设置脚本
-        "-s", str(START),      # 起始帧
-        "-e", str(END),        # 结束帧
+        # "-s", str(START),      # 起始帧
+        # "-e", str(END),        # 结束帧
         "-o", output_path, 
         "-a"                   # 执行动画渲染 (Animation)
     ]
     
-    # 打印一下命令方便检查 (Windows和Linux通用)
+    # 打印一下命令方便检查
     print(f"正在启动: {' '.join(cmd)}")
     
     # 执行命令 (shell=False 更安全，且自动处理路径空格)
